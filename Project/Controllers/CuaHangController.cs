1157b31bf534319f88d1eeab77ee432503a76a94
Longzip113 <longzip113@gmail.com>
2020-12-27 18:31:17 +0700

    Initial commit

---
+++ Project/Controllers/CuaHangController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Project.Controllers
+{
+    public class CuaHangController : Controller
+    {
+        //
+        // GET: /CuaHang/
+
+        //public ActionResult DanhSachGiay()
+        //{
+        //    return View();
+        //}
+
+        public ActionResult DanhSachGiay(String id = "")
+        {
+            return View();
+        }
+
+
+        public ActionResult ChiTietSanPham()
+        {
+            return View();
+        }
+
+        public ActionResult TimKiem(String tenSP = "")
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Project/Controllers/GioHangController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Project.Controllers
+{
+    public class GioHangController : Controller
+    {
+        //
+        // GET: /GioHang/
+
+        public ActionResult GioHang()
+        {
+            return View();
+        }
+
+        public ActionResult ThanhToan()
+        {
+            return View();
+        }
+
+    }
+}
